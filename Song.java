560c6967bada3fa3281b1a1748baab47e2b1f512
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-03 07:35:37 -0700

    add program to print out song chorus

---
+++ Song.java
@@ -0,0 +1,10 @@
+public class Song {
+  public static void main (String[] args) {
+    System.out.println("Here I go again on my own, ");
+    System.out.println("Going down the only road I've even known, ");
+    System.out.println("Like a drifter I was born to walk alone, ");
+    System.out.println("I've made up my mind, ");
+    System.out.println("I ain't wastin' no more time ");
+    System.out.println("So here I go again...");
+  }
+}
