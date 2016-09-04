37d9a15474054fb8327e0f5a22bf4a05e79b774a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-04 14:37:16 -0700

    add program to list what friends should bring to a potluck

---
+++ PotluckFriends.java
@@ -0,0 +1,86 @@
+public class PotluckFriends {
+  public static void main(String[] args) {
+    String[] myFriends={"Chris", "Becky", "Johnathan", "Annie", "Tess", "Amber", "Katy", "Aimen", "Ewa", "Angie", "J'ena", "Kyle", "Stephen", "Adam", "Zach", "Caleb"};
+    for(String friend:myFriends) {
+      System.out.println(friend + " should bring ");
+      if(friend.startsWith("a")||friend.startsWith("A")) {
+        System.out.println("an apple pie.");
+      }
+      if(friend.startsWith("b")||friend.startsWith("B")) {
+        System.out.println("a banana cream pie.");
+      }
+      if(friend.startsWith("c")||friend.startsWith("C")) {
+        System.out.println("chocolate.");
+      }
+      if(friend.startsWith("d")||friend.startsWith("D")) {
+        System.out.println("hot dogs.");
+      }
+      if(friend.startsWith("e")||friend.startsWith("E")) {
+        System.out.println("elephant ears.");
+      }
+      if(friend.startsWith("f")||friend.startsWith("F")) {
+        System.out.println("fried rice.");
+      }
+      if(friend.startsWith("g")||friend.startsWith("G")) {
+        System.out.println("gravy.");
+      }
+      if(friend.startsWith("h")||friend.startsWith("H")) {
+        System.out.println("a ham.");
+      }
+      if(friend.startsWith("i")||friend.startsWith("I")) {
+        System.out.println("ice.");
+      }
+      if(friend.startsWith("j")||friend.startsWith("J")) {
+        System.out.println("juice.");
+      }
+      if(friend.startsWith("k")||friend.startsWith("K")) {
+        System.out.println("kale.");
+      }
+      if(friend.startsWith("l")||friend.startsWith("L")) {
+        System.out.println("lasagne.");
+      }
+      if(friend.startsWith("m")||friend.startsWith("M")) {
+        System.out.println("mashed potatoes.");
+      }
+      if(friend.startsWith("n")||friend.startsWith("N")) {
+        System.out.println("nachos.");
+      }
+      if(friend.startsWith("o")||friend.startsWith("O")) {
+        System.out.println("something with onions.");
+      }
+      if(friend.startsWith("p")||friend.startsWith("P")) {
+        System.out.println("potato salad.");
+      }
+      if(friend.startsWith("q")||friend.startsWith("Q")) {
+        System.out.println("quiche.");
+      }
+      if(friend.startsWith("r")||friend.startsWith("R")) {
+        System.out.println("rolls.");
+      }
+      if(friend.startsWith("s")||friend.startsWith("S")) {
+        System.out.println("some soda pop.");
+      }
+      if(friend.startsWith("t")||friend.startsWith("T")) {
+        System.out.println("a turkey.");
+      }
+      if(friend.startsWith("u")||friend.startsWith("U")) {
+        System.out.println("an upside-down pineapple cake.");
+      }
+      if(friend.startsWith("v")||friend.startsWith("V")) {
+        System.out.println("vegetable.");
+      }
+      if(friend.startsWith("w")||friend.startsWith("W")) {
+        System.out.println("wheat bread.");
+      }
+      if(friend.startsWith("x")||friend.startsWith("X")) {
+        System.out.println("xacuti.");
+      }
+      if(friend.startsWith("y")||friend.startsWith("Y")) {
+        System.out.println("yams.");
+      }
+      if(friend.startsWith("z")||friend.startsWith("Z")) {
+        System.out.println("zucchini.");
+      }
+    }
+  }
+}
