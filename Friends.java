10c9049a43278b9b90d2ec88d95ed780490e285a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-04 14:36:54 -0700

    add program to list firends

---
+++ Friends.java
@@ -0,0 +1,8 @@
+public class Friends {
+  public static void main(String[] args) {
+    String[] myFriends={"Chris", "Becky", "Johnathan", "Annie", "Tess", "Amber", "Katy", "Aimen", "Ewa", "Angie", "J'ena", "Kyle", "Stephen", "Adam", "Zach", "Caleb"};
+    for(String friend:myFriends) {
+      System.out.println(friend + " is my friend.");
+    }
+  }
+}
