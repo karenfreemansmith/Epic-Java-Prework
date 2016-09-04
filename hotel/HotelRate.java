4619d9a1bf1f45a665a66a6b484fc0cd0d12de82
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-04 13:24:23 -0700

    fix boolean problem by using .equals to replace ==

--- hotel/HotelRate.java
+++ hotel/HotelRate.java
@@ -4,23 +4,14 @@ public class HotelRate{
   public static void main(String[] args) {
     Console c = System.console();
 
-    // System.out.println("What season will you be staying with us?");
-    // String season = c.readLine();
-    String season = "summer";
-    
+    System.out.println("What season will you be staying with us?");
+    String season = c.readLine();
+
     System.out.println("Will you be staying on the weekend or a weekday?");
     String dayOfWeek = c.readLine();
 
-    boolean summer = season == "summer\n";
-    boolean weekend = dayOfWeek == "weekend";
-
-    if(season=="summer") {
-      System.out.println("You spelled it right...");
-    } else {
-      System.out.println("Try again...summer seems to be spelled wrong: " + season);
-    }
-    //System.out.println(dayOfWeek + ": " + weekend);
-    // this is not working, both summer and weekend are false for some reason???
+    boolean summer = season.equals("summer");
+    boolean weekend = dayOfWeek.equals("weekend");
 
     if(summer&&weekend) {
       System.out.println("Make sure you pay down your credit card before coming - your stay will be very expensive.");
