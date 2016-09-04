ca96aaefa2679833c073a50766b5db4a20efd61a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-04 13:49:39 -0700

    add program to tell you what to bring to a potluck based on your anem

---
+++ Potluck.java
@@ -0,0 +1,88 @@
+import java.io.Console;
+
+public class Potluck {
+  public static void main(String[] args) {
+    Console c = System.console();
+    System.out.println("What is your name?");
+    String yourName = c.readLine();
+
+    if(yourName.startsWith("a")||yourName.startsWith("A")) {
+      System.out.println("Bring an apple pie.");
+    }
+    if(yourName.startsWith("b")||yourName.startsWith("B")) {
+      System.out.println("Bring a banana cream pie.");
+    }
+    if(yourName.startsWith("c")||yourName.startsWith("C")) {
+      System.out.println("Bring chocolate.");
+    }
+    if(yourName.startsWith("d")||yourName.startsWith("D")) {
+      System.out.println("Bring hot dogs.");
+    }
+    if(yourName.startsWith("e")||yourName.startsWith("E")) {
+      System.out.println("Bring elephant ears.");
+    }
+    if(yourName.startsWith("f")||yourName.startsWith("F")) {
+      System.out.println("Bring fried rice.");
+    }
+    if(yourName.startsWith("g")||yourName.startsWith("G")) {
+      System.out.println("Bring sparkling grape juice.");
+    }
+    if(yourName.startsWith("h")||yourName.startsWith("H")) {
+      System.out.println("Bring a ham.");
+    }
+    if(yourName.startsWith("i")||yourName.startsWith("I")) {
+      System.out.println("Bring ice.");
+    }
+    if(yourName.startsWith("j")||yourName.startsWith("J")) {
+      System.out.println("Bring juice.");
+    }
+    if(yourName.startsWith("k")||yourName.startsWith("K")) {
+      System.out.println("Bring kale.");
+    }
+    if(yourName.startsWith("l")||yourName.startsWith("L")) {
+      System.out.println("Bring lasagne.");
+    }
+    if(yourName.startsWith("m")||yourName.startsWith("M")) {
+      System.out.println("Bring mashed potatoes.");
+    }
+    if(yourName.startsWith("n")||yourName.startsWith("N")) {
+      System.out.println("Bring nachos.");
+    }
+    if(yourName.startsWith("o")||yourName.startsWith("O")) {
+      System.out.println("Bring something with onions.");
+    }
+    if(yourName.startsWith("p")||yourName.startsWith("P")) {
+      System.out.println("Bring potato salad.");
+    }
+    if(yourName.startsWith("q")||yourName.startsWith("Q")) {
+      System.out.println("Bring a quiche.");
+    }
+    if(yourName.startsWith("r")||yourName.startsWith("R")) {
+      System.out.println("Bring rolls.");
+    }
+    if(yourName.startsWith("s")||yourName.startsWith("S")) {
+      System.out.println("Bring soda pop.");
+    }
+    if(yourName.startsWith("t")||yourName.startsWith("T")) {
+      System.out.println("Bring a turkey.");
+    }
+    if(yourName.startsWith("u")||yourName.startsWith("U")) {
+      System.out.println("Bring upside-down pineapple cake.");
+    }
+    if(yourName.startsWith("v")||yourName.startsWith("V")) {
+      System.out.println("Bring a vegetable.");
+    }
+    if(yourName.startsWith("w")||yourName.startsWith("W")) {
+      System.out.println("Bring wheat bread.");
+    }
+    if(yourName.startsWith("x")||yourName.startsWith("X")) {
+      System.out.println("Bring xacuti.");
+    }
+    if(yourName.startsWith("y")||yourName.startsWith("Y")) {
+      System.out.println("Bring yams.");
+    }
+    if(yourName.startsWith("z")||yourName.startsWith("Z")) {
+      System.out.println("Bring zucchini.");
+    }
+  }
+}
